5190d53e2c389922d9fe021d7e79e77ce5213b95
Saksham Kashyap <47538539+kashyap99saksham@users.noreply.github.com>
2019-12-20 12:32:28 +0530

    Update fabonacci_series_using_rec.cpp

--- recursion/fabonacci_series_using_rec.cpp
+++ recursion/fabonacci_series_using_rec.cpp
@@ -21,3 +21,26 @@ int main()
     }
     return 0;
 }
+
+//using memoization version of dynamic programming.....
+
+#include<iostream>
+using namespace std;
+int main()
+ {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    a[1]=a[2]=1;
+	    for(int i=3;i<=n;i++)
+	    {
+	        a[i]=(a[i-1]+a[i-2])%1000000007;                //VERY GOOD AND IMPORTANT APROACH
+	    }
+	    cout<<a[n]<<endl;
+	}
+	return 0;
+}
