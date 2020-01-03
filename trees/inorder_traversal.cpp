9db60387478d8cdc85bfd23c7d012fec1e2fe2e7
Saksham Kashyap <47538539+kashyap99saksham@users.noreply.github.com>
2020-01-03 12:01:36 +0530

    Create inorder_traversal.cpp

---
+++ trees/inorder_traversal.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    struct node* next,*prev;
+};
+struct node* create()
+{
+    int x;
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    cout<<" Enter The Value (-1 for end) ";
+    cin>>x;
+    if(x == -1)
+        return NULL;
+    temp->data = x;
+    cout<<"Enter Left Child of "<<x;
+    temp->prev = create();
+    cout<<"Enter Right Child of "<<x;
+    temp->next = create();
+    return temp;    
+}
+void preorder(struct node *root)
+{
+    if(root==NULL)
+        return;
+    preorder(root->prev);
+    cout<<root->data;
+    preorder(root->next);
+}
+int main()
+{
+    struct node *root;
+    root = create();
+    preorder(root);
+}
