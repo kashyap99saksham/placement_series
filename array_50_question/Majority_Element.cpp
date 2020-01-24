7ffd1232f9b14edd560f75b04ba81fd15ba79e85
Saksham Kashyap <47538539+kashyap99saksham@users.noreply.github.com>
2020-01-24 15:21:01 +0530

    Update Majority_Element.cpp

--- array_50_question/Majority_Element.cpp
+++ array_50_question/Majority_Element.cpp
@@ -1,30 +1,78 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int test;
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int test;
+//     cin>>test;
+//     while(test--)
+//     {
+//         int size;
+//         cin>>size;
+//         // int *arr = new int[size];
+//         vector <int> v;
+//         for (int i = 0; i < size; i++)
+//             cin>>v[i];
+//         map<int,int> m;
+//         for (int i = 0; i < size; i++)
+//         {   
+//             if (m.find(v[i]) != m.end()) 
+//             {
+//                 m[v[i]] = m[v[i]]+1;
+//             } 
+//             else 
+//             {
+//                 m[v[i]] = 0;
+//             }
+//         }
+//         cout<<m[3];
+// 	}
+//         cout<<endl;
+// }
+
+
+// NOT RUNNING IN G4G IDE
+/* C++ program for finding out majority 
+element in an array */
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void findMajority(int arr[], int size) 
+{ 
+	unordered_map<int, int> m; 
+	for(int i = 0; i < size; i++) 
+		m[arr[i]]++; 
+	int count = 0; 
+	for(auto i : m) 
+	{ 
+		if(i.second > size / 2) 
+		{ 
+			count =1; 
+			cout<< i.first<<endl; 
+			break; 
+		} 
+	} 
+	if(count == 0) 
+		cout << -1 << endl; 
+} 
+
+// Driver code 
+int main() 
+{ 
+   int test;
     cin>>test;
     while(test--)
     {
         int size;
         cin>>size;
-        // int *arr = new int[size];
-        vector <int> v;
+        int *arr = new int[size];
+        // vector <int> v;
+        int val;
         for (int i = 0; i < size; i++)
-            cin>>v[i];
-        map<int,int> m;
-        for (int i = 0; i < size; i++)
-        {   
-            if (m.find(v[i]) != m.end()) 
-            {
-                m[v[i]] = m[v[i]]+1;
-            } 
-            else 
-            {
-                m[v[i]] = 0;
-            }
+        {
+            cin>>arr[i];
         }
-        cout<<m[3];
-	}
-        cout<<endl;
-}
+
+	    findMajority(arr, size);
+    }
+	return 0; 
+} 
