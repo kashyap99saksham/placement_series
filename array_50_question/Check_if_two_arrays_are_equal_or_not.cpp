b22d165d9680447319ae0f7a6f06b6cb4b8fceeb
Saksham Kashyap <47538539+kashyap99saksham@users.noreply.github.com>
2020-02-01 23:46:46 +0530

    Update Check_if_two_arrays_are_equal_or_not.cpp

--- array_50_question/Check_if_two_arrays_are_equal_or_not.cpp
+++ array_50_question/Check_if_two_arrays_are_equal_or_not.cpp
@@ -1,3 +1,43 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int test;
+//     cin>>test;
+//     while(test--)
+//     {
+//         int size;
+//         cin>>size;
+//         int arr[size],brr[size];
+
+//         // TAKING BOTH ARRAY
+//         for(int i=0;i<size;i++)
+//             cin>>arr[i];
+//         for(int i=0;i<size;i++)
+//             cin>>brr[i];
+        
+//         // SORTING
+//         bool flag = true;
+//         sort(arr,arr+size);     //O(nlogn)
+//         sort(brr,brr+size);
+//         for (int i = 0; i < size; i++)
+//         {
+//             if(arr[i]!=brr[i])
+//             {
+//                 flag = false;
+//                 cout<<0;
+//                 break;
+//             }
+//         }
+//         if(flag==true)
+//             cout<<1;
+                
+//         cout<<endl;
+//     }
+
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
@@ -8,21 +48,29 @@ int main()
     {
         int size;
         cin>>size;
-        int arr[size],brr[size];
-
+        int arr[size];
+        unordered_map <int,int> m1;
         // TAKING BOTH ARRAY
         for(int i=0;i<size;i++)
-            cin>>arr[i];
-        for(int i=0;i<size;i++)
-            cin>>brr[i];
-        
-        // SORTING
-        bool flag = true;
-        sort(arr,arr+size);
-        sort(brr,brr+size);
-        for (int i = 0; i < size; i++)
         {
-            if(arr[i]!=brr[i])
+            cin>>arr[i];
+        }
+        for(int i=0;i<size;i++)
+        {
+            int v;  cin>>v;
+            m1[v]++;
+        }
+        for(int i=0;i<size;i++)
+        {
+            if(m1.find(arr[i])!=m1.end())
+            {
+                m1[arr[i]]--;
+            }
+        }
+        bool flag = true;
+        for(auto i:m1)
+        {
+            if(i.second!=0)
             {
                 flag = false;
                 cout<<0;
@@ -31,8 +79,5 @@ int main()
         }
         if(flag==true)
             cout<<1;
-                
-        cout<<endl;
     }
-
 }
