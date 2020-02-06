f6b97e72d9b12ee8595f2989c3a4c244198533a8
kashyap99saksham <kprincekashyap7@gmail.com>
2020-02-06 19:04:18 +0530

    traverse_cLL

---
+++ linked_list/Circular Linked List Traversal.cpp
@@ -0,0 +1,13 @@
+struct Node *temp = first;  
+  
+    // If linked list is not empty 
+    if (first != NULL)  
+    { 
+        // Keep printing nodes till we reach the first node again 
+        do
+        { 
+            printf("%d ", temp->data); 
+            temp = temp->next; 
+        } 
+        while (temp != first); 
+    } 
