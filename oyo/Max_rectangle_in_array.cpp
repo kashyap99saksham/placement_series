0145ff582bd5ae723429947e7219223e667c9c00
kashyap99saksham <kprincekashyap7@gmail.com>
2020-02-07 14:10:18 +0530

    i m msg

--- oyo/Max_rectangle_in_array.cpp
+++ oyo/Max_rectangle_in_array.cpp
@@ -1,99 +1,109 @@
-#include<bits/stdc++.h>
-using namespace std;
-long long int getMaxArea(long long int hist[],long long int n) 
+// C++ program to find largest rectangle with all 1s 
+// in a binary matrix 
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// Rows and columns in input matrix 
+#define R 4 
+#define C 4 
+
+// Finds the maximum area under the histogram represented 
+// by histogram. See below article for details. 
+// https://www.geeksforgeeks.org/largest-rectangle-under-histogram/ 
+int maxHist(int row[]) 
 { 
-    // Create an empty stack. The stack holds indexes  
-    // of hist[] array. The bars stored in stack are  
-    // always in increasing order of their heights. 
-    stack<int> s; 
-  
-    long long int max_area = 0; // Initialize max area 
-    long long int tp;  // To store top of stack 
-    long long int area_with_top; // To store area with top bar 
-                       // as the smallest bar 
-  
-    // Run through all bars of given histogram 
-    long long int i = 0; 
-    while (i < n) 
-    { 
-        // If this bar is higher than the bar on top  
-        // stack, push it to stack 
-        if (s.empty() || hist[s.top()] <= hist[i]) 
-            s.push(i++); 
-  
-        // If this bar is lower than top of stack,  
-        // then calculate area of rectangle with stack  
-        // top as the smallest (or minimum height) bar.  
-        // 'i' is 'right index' for the top and element  
-        // before top in stack is 'left index' 
-        else
-        { 
-            tp = s.top();  // store the top index 
-            s.pop();  // pop the top 
-  
-            // Calculate the area with hist[tp] stack  
-            // as smallest bar 
-            area_with_top = hist[tp] * (s.empty() ? i :  
-                                   i - s.top() - 1); 
-  
-            // update max area, if needed 
-            if (max_area < area_with_top) 
-                max_area = area_with_top; 
-        } 
-    } 
-  
-    // Now pop the remaining bars from stack and calculate 
-    // area with every popped bar as the smallest bar 
-    while (s.empty() == false) 
-    { 
-        tp = s.top(); 
-        s.pop(); 
-        area_with_top = hist[tp] * (s.empty() ? i :  
-                                i - s.top() - 1); 
-  
-        if (max_area < area_with_top) 
-            max_area = area_with_top; 
-    } 
-  
-    return max_area; 
+	// Create an empty stack. The stack holds indexes of 
+	// hist[] array/ The bars stored in stack are always 
+	// in increasing order of their heights
+	stack<int> result; 
+
+	int top_val;	 // Top of stack 
+
+	int max_area = 0; // Initialize max area in current 
+					// row (or histogram) 
+
+	int area = 0; // Initialize area with current top 
+
+	// Run through all bars of given histogram (or row) 
+	int i = 0; 
+	while (i < C) 
+	{ 
+		// If this bar is higher than the bar on top stack, 
+		// push it to stack 
+		if (result.empty() || row[result.top()] <= row[i]) 
+			result.push(i++); 
+
+		else
+		{ 
+			// If this bar is lower than top of stack, then 
+			// calculate area of rectangle with stack top as 
+			// the smallest (or minimum height) bar. 'i' is 
+			// 'right index' for the top and element before 
+			// top in stack is 'left index' 
+			top_val = row[result.top()]; 
+			result.pop(); 
+			area = top_val * i; 
+
+			if (!result.empty()) 
+				area = top_val * (i - result.top() - 1 ); 
+			max_area = max(area, max_area); 
+		} 
+	} 
+
+	// Now pop the remaining bars from stack and calculate area 
+	// with every popped bar as the smallest bar 
+	while (!result.empty()) 
+	{ 
+		top_val = row[result.top()]; 
+		result.pop(); 
+		area = top_val * i; 
+		if (!result.empty()) 
+			area = top_val * (i - result.top() - 1 ); 
+
+		max_area = max(area, max_area); 
+	} 
+	return max_area; 
 } 
-int maxRectangle(int arr[][],int size)
-{
-    int brr[size];
-    int z=0;
-    for (int j = 0; j < size; j++)
-    {
-        brr[z++]=arr[0][j];
-    }
-    int maxi = INT_MIN;
-    maxi = max(maxi,magetMaxArea(brr,size));
+// dcsdc
 
-    for(int i=1;i<size;i++)
-    {
-        z=0;
-        for (int j = 0; j < size; j++)
-        {
-            brr[z]=arr[i][j];
-            brr[z]++;
-            z++;
-        }
-        getMaxArea(brr,size);
-    }
-}
-int main()
-{
-    int test;
-    cin>>test;
-    while(test--)
-    {
-        long long int row,col;
-        cin>>row>>col;
-        long long int arr[row][col];
-        for (long long int  i = 0; i < row; i++)
-            for (long long int j = 0; i < col; j++)
-                cin>>arr[row][col];
+// Returns area of the largest rectangle with all 1s in A[][] 
+int maxRectangle(int A[][C]) 
+{ 
+	// Calculate area for first row and initialize it as 
+	// result 
+	int result = maxHist(A[0]); 
 
-        cout<<maxRectangle(arr,row,col);
-        cout<<endl;
-    }
-}
+	// iterate over row to find maximum rectangular area 
+	// considering each row as histogram 
+	for (int i = 1; i < R; i++) 
+	{ 
+
+		for (int j = 0; j < C; j++) 
+
+			// if A[i][j] is 1 then add A[i -1][j] 
+			if (A[i][j]) 
+                A[i][j] += A[i - 1][j]; 
+
+
+		// Update result if area with current row (as last row) 
+		// of rectangle) is more 
+		result = max(result, maxHist(A[i])); 
+	} 
+
+	return result; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int A[][C] = { {0, 1, 1, 0}, 
+				{1, 1, 1, 1}, 
+				{1, 1, 1, 1}, 
+				{1, 1, 0, 0}, 
+				}; 
+
+	cout << "Area of maximum rectangle is "
+		<< maxRectangle(A); 
+
+	return 0; 
+} 
